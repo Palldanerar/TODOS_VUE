095a09d639be38fb84e27b0f3712ba8d6d39461b
Palldanerar <alexkharin86@gmail.com>
2023-10-26 09:25:02 +0300

    template

---
+++ src/components/AppTodoItem.vue
@@ -0,0 +1,29 @@
+<script lang="ts">
+import { PropType, defineComponent } from "vue"
+import {ITodo} from "@/interface/ITodo"
+
+
+export default defineComponent({
+    props: {
+        todo: {
+            type: Object as PropType<ITodo>,
+            required: true
+        }
+    }
+})
+
+</script>
+
+<template>
+    <li class="todo-item" :class="{'todo-item--done' : todo.completed}">
+        <div class="todo-item__status">
+            <i class="bi bi-check2"></i>
+        </div>
+        <span class="todo-item__text">{{ todo.text }}</span>
+        <button class="todo-item__remove-button">
+            <i class="bi bi-trash3"></i>
+        </button>
+    </li>
+</template>
+
+<style></style>
