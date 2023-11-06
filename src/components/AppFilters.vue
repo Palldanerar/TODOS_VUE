fad2988544eabc3cff34e6fedb011d0be7431733
Palldanerar <alexkharin86@gmail.com>
2023-11-06 11:52:43 +0300

    final version

--- src/components/AppFilters.vue
+++ src/components/AppFilters.vue
@@ -1,13 +1,33 @@
 <script lang="ts">
+import { PropType, defineComponent } from 'vue';
 
+export default defineComponent({
+    props: {
+        activeFilter: {
+            type: String as PropType<"All" | "Active" | "Done">,
+            required: true
+        }
+    },
+    data() {
+        return {
+            filters: ["All", "Active", "Done"]
+        }
+    },
+    methods: {
+        setFilter(filter: "All" | "Active" | "Done") {
+            this.$emit('setFilter', filter)
+        }
+    },
+    emits: {
+        setFilter: (filter: "All" | "Active" | "Done") => filter
+    }
+})
 </script>
 
 <template>
     <aside class="app-filters">
         <section class="toggle-group">
-            <button class="button button--primary">All</button>
-            <button class="button">Active</button>
-            <button class="button">Done</button>
+            <button v-for="filter in filters" :key="filter" class="button" value="All" :class="{'button--primary' : activeFilter == filter}" @click="setFilter(filter)">{{ filter }}</button>
         </section>
     </aside>
 </template>
